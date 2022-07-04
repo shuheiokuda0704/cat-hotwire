019ebd21f7b35dc515af1f33a65bd2b3124125df
shuheiokuda0704 <shuheiokuda0704@gmail.com>
2022-07-05 04:13:42 +0900

    Enable turbo-stream for editing cat

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -35,6 +35,7 @@ class CatsController < ApplicationController
   # PATCH/PUT /cats/1
   def update
     if @cat.update(cat_params)
+      # リダイレクトを削除（リダイレクトがないと暗黙的に`render`が実行される）
       redirect_to @cat, notice: "ねこを更新しました。"
     else
       render :edit, status: :unprocessable_entity
